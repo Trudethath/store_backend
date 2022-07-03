67f33d1e6344cba0d6f6c018936aee9c625bb836
Trudethath <bartekkuleta21@gmail.com>
2022-07-03 14:18:47 +0200

    changed UsersExist status code from 400 to 409

--- src/users/exceptions/UserAlreadyExists.exception.ts
+++ src/users/exceptions/UserAlreadyExists.exception.ts
@@ -2,6 +2,6 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class UserAlreadyExistsException extends HttpException {
   constructor(msg?: string, status?: HttpStatus) {
-    super(msg || 'User already exists!', status || HttpStatus.BAD_REQUEST);
+    super(msg || 'User already exists!', status || HttpStatus.CONFLICT);
   }
 }
